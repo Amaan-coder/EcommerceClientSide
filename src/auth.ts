a4dfba684d7580beadaa3a467d677b5789df69e5
Mohd Amaan <mohd.amaan45@gmail.com>
2024-04-08 02:40:01 +0530

    auth and environment file set

---
+++ src/auth.ts
@@ -0,0 +1,4 @@
+import { environment } from "./environments/environment";
+
+export const GET_ALL_DETAILS = environment.domain +"/api/products"
+export const GET_ALL_CATEGORY = environment.domain +"/api/category"
